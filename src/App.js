13a52526e5acc627a9ea3ed6422919c97a4ee198
Thibault HENTGES <thibault.hentges@gmail.com>
2018-06-26 03:50:46 +0200

    page working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-        <div>testing deploying a react app on github pages</div>
+        <div>This is my react app on github pages ! <br /> more content incomming... </div>
     );
   }
 }
