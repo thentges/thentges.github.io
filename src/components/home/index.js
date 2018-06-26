d92f5913bafbcb6eb09e924a046e3d9eceff357e
Thibault HENTGES <thibault.hentges@gmail.com>
2018-06-26 17:55:17 +0200

    adding ipsum

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -17,6 +17,14 @@ class Home extends Component {
                 <h1>
                      { t('home.title', { name: "Thibault" }) }
                 </h1>
+                <div className="about">
+                    You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.
+                    <br />
+                    <div className="quote">
+                        {'Samuel L Ipsum from '}
+                        <a href="http://slipsum.com/">{'http://slipsum.com/'}</a>
+                    </div>
+                </div>
                 <div>
                     <Button primary>{t('home.buttons.primary')}</Button>
                     <Button secondary>{t('home.buttons.secondary')}</Button>
