64eff98fa1c5200c00aa92fc57b3c0a643ee89e7
Thibault HENTGES <thibault.hentges@gmail.com>
2018-06-26 16:09:46 +0200

    removing reset.scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Home from './components/home'
-import './styles/semantic.scss'
 import style from './styles/app.scss'
 
 class App extends Component {
