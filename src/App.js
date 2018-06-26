54f1a9a5632a626efcccee9a60265678caba7c2e
Thibault HENTGES <thibault.hentges@gmail.com>
2018-06-26 04:03:11 +0200

    sass support

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import style from './styles/app.scss'
 
 class App extends Component {
   render() {
     return (
-        <div>This is my react app on github pages ! <br /> more content incomming... </div>
+        <React.Fragment>
+            <div>
+                This is my react app on github pages ! <br />
+                more content incomming...
+            </div>
+            <div className="test-sass">
+                testing sass support, if this text is red, that means sass is working
+            </div>
+        </React.Fragment>
     );
   }
 }
