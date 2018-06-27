99cb1d530cfcd8825928dc42d9472b07de05eef3
thentges <thibault.hentges@gmail.com>
2018-06-27 23:41:38 +0200

    starting point

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,6 +4,7 @@ import { Button } from 'semantic-ui-react'
 import logo from '../../images/thentges_logo.png'
 import { translate } from 'react-i18next';
 import Tags from '../ui/tags'
+import HomeNav from './navigation'
 
 class Home extends Component {
     render() {
@@ -15,18 +16,12 @@ class Home extends Component {
                 <div>
                     <img className="logo" src={logo} alt={'thentges avatar'} />
                 </div>
-                <h1>
-                     { t('home.title', { name: "Thibault" }) }
-                </h1>
+                <h1 className="title">Thibault HENTGÈS</h1>
+                <div className="subtitle">Full Stack blabla</div>
                 <div className="about">
-                    You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.
-                    <br />
-                    <div className="quote">
-                        {'Samuel L Ipsum from '}
-                        <a href="http://slipsum.com/">{'http://slipsum.com/'}</a>
-                    </div>
-                    <Tags list={["TAG1", "TAG2", "TAG3"]} />
+                    blablablablablabla
                 </div>
+                <HomeNav />
                 <div className="buttons">
                     <Button primary>{t('home.buttons.primary')}</Button>
                     <Button secondary>{t('home.buttons.secondary')}</Button>
---
+++ src/components/home/navigation/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class HomeNav extends React.Component {
+    render(){
+        return (
+            <div>c'est la home nav mon poto sur !</div>
+        )
+    }
+}
+
+export default HomeNav;
