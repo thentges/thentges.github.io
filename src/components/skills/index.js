9d1d7c5a420fc3ef5acf60bdb630d1cd1cd763da
Thibault HENTGES <thibault.hentges@gmail.com>
2018-06-27 02:15:50 +0200

    backend start

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -3,6 +3,7 @@ import style from './style.scss'
 import BlockTitle from '../ui/block_title'
 import SubTitle from '../ui/sub_title'
 import { translate } from 'react-i18next';
+import BackEndSkills from './backend'
 
 class Skills extends Component {
     render() {
@@ -10,7 +11,9 @@ class Skills extends Component {
         return (
             <div className={style.component}>
                 <BlockTitle color='white'>{t('skills.title')}</BlockTitle>
-                <SubTitle color='white'>{t('skills.backend')}</SubTitle>
+                <SubTitle color='white'>Back-end</SubTitle>
+                    <BackEndSkills />
+                <SubTitle color='white'>Front-end</SubTitle>
             </div>
         );
     }
