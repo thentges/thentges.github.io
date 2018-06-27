42c167b4cc1d389605104ded864e34b1100cb74f
thentges <thibault.hentges@gmail.com>
2018-06-28 01:43:06 +0200

    about + tags ok

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -6,25 +6,36 @@ import { translate } from 'react-i18next';
 import Tags from '../ui/tags'
 import HomeNav from './navigation'
 
+// <div className="buttons">
+//     <Button primary>{t('home.buttons.primary')}</Button>
+//     <Button secondary>{t('home.buttons.secondary')}</Button>
+// </div>
+
+
 class Home extends Component {
     render() {
 
         const { t } = this.props;
+        const tag_list = [
+            t('tags.engineer'), t('tags.dev'), t('tags.web'), t('tags.full_stack'),
+            t('tags.best_practices'), t('tags.back')
+        ]
 
         return (
             <div className={style.component}>
-                <div>
-                    <img className="logo" src={logo} alt={'thentges avatar'} />
-                </div>
-                <h1 className="title">Thibault HENTGÈS</h1>
-                <div className="subtitle">Full Stack blabla</div>
-                <div className="about">
-                    blablablablablabla
-                </div>
-                <HomeNav />
-                <div className="buttons">
-                    <Button primary>{t('home.buttons.primary')}</Button>
-                    <Button secondary>{t('home.buttons.secondary')}</Button>
+                <div className="flex-container">
+                    <div>
+                        <img className="logo" src={logo} alt={'thentges avatar'} />
+                    </div>
+                    <h1 className="title">Thibault HENTGÈS</h1>
+                    <div className="subtitle">
+                        {t('home.subtitle')}<a href='https://www.isep.fr/'>ISEP</a> (Paris, France)
+                    </div>
+                    <div className="about">
+                        <div>{t('home.about')}</div>
+                        <Tags className='tags' list={tag_list} />
+                    </div>
+                    <HomeNav />
                 </div>
             </div>
         );
