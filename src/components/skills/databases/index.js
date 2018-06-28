d50236d23afc5e482d68357cf111d3723267618f
thentges <thibault.hentges@gmail.com>
2018-06-28 19:04:52 +0200

    adding ORMs

--- src/components/skills/databases/index.js
+++ src/components/skills/databases/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Card from '../../ui/card';
 import mysql_img from '../../../images/mysql.svg'
 import postgres_img from '../../../images/postgres.svg'
-import mongo_img from '../../../images/mongo.svg'
+// import mongo_img from '../../../images/mongo.svg'
+// <Card name='mongoDB' image={mongo_img} />
+import doctrine_img from '../../../images/doctrine.svg'
+import hibernate_img from '../../../images/hibernate.svg'
+import sequelize_img from '../../../images/sequelize.svg'
 
 class DataBaseCards extends React.Component {
     render() {
@@ -10,7 +14,9 @@ class DataBaseCards extends React.Component {
             <div className='cards-container'>
                 <Card name='mySQL' image={mysql_img} />
                 <Card name='postgreSQL' image={postgres_img} />
-                <Card name='mongoDB' image={mongo_img} />
+                <Card name='Doctrine (ORM)' image={doctrine_img} />
+                <Card name='Hibernate (ORM)' image={hibernate_img} />
+                <Card name='Sequelize (ORM)' image={sequelize_img} />
             </div>
         )
     }
