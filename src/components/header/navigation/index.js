3c2e0172506c82bf93c10a304ecf0160c6f4f19e
thentges <thibault.hentges@gmail.com>
2018-06-30 02:49:37 +0200

    refactor laptop menu

--- src/components/header/navigation/index.js
+++ src/components/header/navigation/index.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import { translate } from 'react-i18next';
 import style from './style.scss'
-import Item from '../item'
-import logo from '../../../images/thentges_logo.png'
+import LaptopNav from './laptop'
 
 class Navigation extends React.Component {
     render() {
         const {t} = this.props;
         return (
             <div className={style.component}>
-                <img className="logo" src={logo} alt={'thentges avatar'}
-                    onClick={this.props.goTo.home} />
-
-                <div className="nav">
-                    <Item isSelected={this.props.currentPage === 'skills'}
-                        handleClick={this.props.goTo.skills}>
-                        {t('nav.skills')}
-                    </Item>
-                    <Item isSelected={this.props.currentPage === 'exp'}
-                        handleClick={this.props.goTo.exp}>
-                        {t('nav.exp')}
-                    </Item>
-                </div>
+                <LaptopNav goTo={this.props.goTo} currentPage={this.props.currentPage} />
             </div>
         );
     }
 }
 
-export default translate('common')(Navigation);
+export default Navigation;
---
+++ src/components/header/navigation/laptop/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Item from '../../item'
+import logo from '../../../../images/thentges_logo.png'
+import { translate } from 'react-i18next';
+
+class LaptopNav extends React.Component {
+    render() {
+        const {t} = this.props
+
+        return (
+            <div className="laptop-menu">
+                <img className="logo" src={logo} alt={'thentges avatar'}
+                    onClick={this.props.goTo.home} />
+
+                <div className="nav">
+                    <Item isSelected={this.props.currentPage === 'skills'}
+                        handleClick={this.props.goTo.skills}>
+                        {t('nav.skills')}
+                    </Item>
+                    <Item isSelected={this.props.currentPage === 'exp'}
+                        handleClick={this.props.goTo.exp}>
+                        {t('nav.exp')}
+                    </Item>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default translate('common')(LaptopNav);
