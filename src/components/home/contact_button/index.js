5c1898379556c644169fd2d44eeaa70dd289c836
thentges <thibault.hentges@gmail.com>
2018-07-12 04:28:36 +0200

    tooltip as a component

--- src/components/home/contact_button/index.js
+++ src/components/home/contact_button/index.js
@@ -3,6 +3,7 @@ import style from './style.scss'
 import Send from 'react-feather/dist/icons/send'
 import {CopyToClipboard} from 'react-copy-to-clipboard'
 import { translate } from 'react-i18next';
+import Tooltip from '../../ui/tooltip'
 
 class ContactButton extends React.Component {
     constructor(props){
@@ -25,20 +26,13 @@ class ContactButton extends React.Component {
         const {t} = this.props
         const email = 'thibault.hentges@gmail.com';
 
-        const tooltip_css = this.state.isMailCopied ? 'visible' : 'hidden';
-        const tooltip = (
-            <span className={"tooltip " + tooltip_css}>
-                {t('home.copied')}
-            </span>
-        )
-
         const component = window.navigator.maxTouchPoints === 0 ?
         (
             <CopyToClipboard text={email} onCopy={this.copy.bind(this)}>
                 <div className={style.component}>
                         <span className='contact'>contact <Send className="icon" size={15}/> </span>
                         <span className='mail'>{email}</span>
-                        {tooltip}
+                        <Tooltip show={this.state.isMailCopied} text={t('home.copied')} />
                 </div>
             </CopyToClipboard>
         ) :
